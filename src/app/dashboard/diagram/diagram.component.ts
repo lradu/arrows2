4cef3733aa23a871e8088c54be3f9f8c260432f6
Radu Lucut <lucutradu@gmail.com>
2017-02-01 21:58:49 +0200

    curved arrows, mirror node

--- src/app/dashboard/diagram/diagram.component.ts
+++ src/app/dashboard/diagram/diagram.component.ts
@@ -20,6 +20,7 @@ export class DiagramComponent implements OnInit {
 	public gRelationships: any;
 	public zoom: any;
 	public model: Model;
+	public groups: any;
 
 	public dbref: any;
 	public user: any;
@@ -30,6 +31,7 @@ export class DiagramComponent implements OnInit {
 
 	public showTools: boolean;
 	public showNodeTools: boolean;
+	public mirrorNode: any;
 
 	constructor(
 		private af: AngularFire, 
@@ -38,6 +40,12 @@ export class DiagramComponent implements OnInit {
 		) {
 			this.dbref = firebase.database().ref();
 			this.user = firebase.auth().currentUser;
+			this.mirrorNode = {
+				"fill": "white",
+				"color": "#292b2c",
+				"isRectangle": false,
+				"isLocked": false
+			};
 	}
 
 	ngOnInit(){
@@ -50,7 +58,7 @@ export class DiagramComponent implements OnInit {
 		this.gNodes = this.svg.append("g")
 			.attr("class", "layer nodes");
 		this.gCaptions = this.svg.append("g")
-			.attr("class", "caption");
+			.attr("class", "layer caption");
 		this.gOverlay = this.svg.append("g")
 			.attr("class", "layer overlay");
 		this.model = new Model();
@@ -93,7 +101,9 @@ export class DiagramComponent implements OnInit {
 							if(snapShot.ref.parent.key == this.currentDiagram){
 								this.model.load(snapShot.val());
 								d3.selectAll('svg > g > *').remove();
+								this.renderRelationships();
 								this.renderNodes();
+								this.renderOverlay();
 								if(zoomFit){
 									this.zoomFit();
 									zoomFit = false;
@@ -109,25 +119,6 @@ export class DiagramComponent implements OnInit {
 		
 	*/
 	renderNodes(){
-		let that = this;
-		let closestNode
-		let newNode = new Node();
-		this.gNodes.selectAll("rect.newNode")
-			.data([newNode])
-			.remove()
-			.enter()
-				.append("rect")
-					.attr("class", "newNode")
-					.attr("width", newNode.radius * 2)
-					.attr("height", newNode.radius * 2)
-					.attr("fill", "none");
-		this.gRelationships.selectAll("path.newRelationship")
-			.data([newNode])
-			.remove()
-			.enter()
-			.append("path")
-				.attr("class", "newRelationship");
-
 		let nodes = this.gNodes.selectAll("rect.node")
 		  .data(this.model.nodes)
 		nodes.remove()
@@ -146,6 +137,7 @@ export class DiagramComponent implements OnInit {
 		   	.style("color", function(node) { return node.style.color });
 		nodes.enter()
 			.append("path")
+				.attr("class", "node properties")
 				.attr("transform", function(node) {
 					return "translate("
 					+ (node.x + 2 * node.radius)
@@ -155,18 +147,19 @@ export class DiagramComponent implements OnInit {
 				})
 				.attr("d", function(node){
 					if(node.properties.text){
+						let l = node.properties.text.split("\n").length;
 						return "M 0 0" +
 						"L 20 -10" +
-						"L 20 -25" +
-						"A 10 10 0 0 1 30 -35" +
-			 			"L " + (node.properties.width * 2)  + " -35" +
-						"A 10 10 0 0 1 " + (node.properties.width * 2 + 10) + " -25" +
-						"L " + (node.properties.width * 2 + 10) + " 25" +
-						"A 10 10 0 0 1 " + (node.properties.width * 2) + " 35" +
-						"L 30 35" +
-						"A 10 10 0 0 1 20 25" +
+						"L 20 " + -(l * 25) +
+						"A 10 10 0 0 1 30" + -(l * 25 + 10) +
+			 			"L " + (node.properties.width * 2)  + " " + -(l * 25 + 10) +
+						"A 10 10 0 0 1 " + (node.properties.width * 2 + 10) + " " + -(l * 25) +
+						"L " + (node.properties.width * 2 + 10) + " " + (l * 25) +
+						"A 10 10 0 0 1 " + (node.properties.width * 2) + " " + (l * 25 + 10) +
+						"L 30 " + (l * 25 + 10) +
+						"A 10 10 0 0 1 20 " + (l * 25) +
 						"L 20 10" +
-						"Z"
+						"Z";
 					}
 				})
 				.attr("fill", "white")
@@ -196,11 +189,63 @@ export class DiagramComponent implements OnInit {
 				.attr("font-size",  "50px")
 				.attr("alignment-baseline", "central")
 				.text(function(node) { return node.caption; })
+	}
+	renderRelationships(){
+		let that = this;
+		this.groupRelationships();
 
-		let overlays = this.gOverlay.selectAll("rect.node")
+		let rel = this.gRelationships.selectAll("g.groups")
+			.data(that.groups);
+		rel.remove();
+		rel.enter()
+			.append("g")
+				.attr("class", "groups")
+		 	  .selectAll("g.relationships")
+				.data(function(d) { return d; })
+				.remove()
+		 	  .enter()
+				.append("path")
+					.attr("class", "relationships")
+					.attr("transform", function(rl) {
+						return "translate("
+						+ (rl.source.x + rl.source.radius)
+						+ ","
+						+ (rl.source.y + rl.source.radius)
+						+ ")" + "rotate(" + rl.angle + ")";
+					})
+					.attr("d", function(rl) {
+						if(rl.offset){
+							return that.curvedArrow(rl.source.radius + 12, rl.target.radius, rl.distance, rl.offset, 5, 20, 20).outline;
+						} else {
+							return that.horizontalArrow(rl.source.radius + 12, rl.distance - rl.target.radius, 5).outline;
+						}
+					})
+					.attr("fill", function(rl) { return rl.style.fill; });    	
+	}
+	renderOverlay(){
+		let that = this;
+		let closestNode
+		let newNode = new Node();
+		this.gNodes.selectAll("rect.newNode")
+			.data([newNode])
+			.remove()
+			.enter()
+				.append("rect")
+					.attr("class", "newNode")
+					.attr("width", newNode.radius * 2)
+					.attr("height", newNode.radius * 2)
+					.attr("fill", "none");
+		this.gRelationships.selectAll("path.newRelationship")
+			.data([newNode])
+			.remove()
+			.enter()
+			.append("path")
+				.attr("class", "newRelationship");
+
+		let nodeOverlays = this.gOverlay.selectAll("rect.node")
 			.data(this.model.nodes)
-		overlays.remove()
-		overlays.enter()
+		nodeOverlays.remove()
+		nodeOverlays.enter()
 			.append("rect")
 				.attr("class", "node")
 				.attr("width", function(node) { return node.radius * 2 + 4; })
@@ -215,13 +260,53 @@ export class DiagramComponent implements OnInit {
 			  .on("click", (node) => {
 			  	if(this.access != "Read Only"){
 				   	this.currentNode = node;
+				   	this.copyMirrorNode();
 				   	this.showNodeTools = true;
-				  	this.showTools = true;
+				  	if(!this.mirrorNode.isLocked) { this.showTools = true; }
 				  	this.ref.detectChanges();
 			  	}
 			   })
 			  .call(d3.drag()
 			     .on("drag", dragged));
+
+		let relOverlays = this.gOverlay.selectAll("g.groups")
+			.data(that.groups);
+		relOverlays.remove();
+		relOverlays.enter()
+			.append("g")
+				.attr("class", "groups")
+		 	  .selectAll("g.relationships")
+				.data(function(d) { return d; })
+				.remove()
+		 	  .enter()
+				.append("path")
+					.attr("class", "relationships")
+					.attr("transform", function(rl) {
+						return "translate("
+						+ (rl.source.x + rl.source.radius)
+						+ ","
+						+ (rl.source.y + rl.source.radius)
+						+ ")" + "rotate(" + rl.angle + ")";
+					})
+					.attr("d", function(rl) {
+						if(rl.offset){
+							return that.curvedArrow(rl.source.radius + 12, rl.target.radius, rl.distance, rl.offset, 5, 20, 20).outline;
+						} else {
+							return that.horizontalArrow(rl.source.radius + 12, rl.distance - rl.target.radius, 5).outline;
+						}
+					})
+					.attr("stroke-width", 5)
+					.attr("stroke", "rgba(255, 255, 255, 0)")
+					.on("mouseover", mRingOver)
+					.on("mouseleave", mRingLeave)
+					.on("click", (rl) => {
+				  	if(this.access != "Read Only"){
+					   	this.currentR = rl;
+					   	this.showNodeTools = false;
+					  	this.showTools = true;
+					  	this.ref.detectChanges();
+				  	}
+				   });
 
 		let nodeRing = this.gOverlay.selectAll('rect.ring')
 			.data(this.model.nodes)
@@ -244,96 +329,6 @@ export class DiagramComponent implements OnInit {
 				   .on("drag", dragRing)
 				   .on('end', dragEndRing));
 		
-		let rel = this.gRelationships.selectAll("path.relationship")
-			.data(this.model.relationships);
-		rel.remove();
-		rel.enter()
-			.append("path")
-				.attr("class", "relationship")
-				.attr("transform", function(rl) {
-					let source = that.model.nodes.find( x => x.id == rl.startNode)
-					let target = that.model.nodes.find( x => x.id == rl.endNode)
-					let angle = source.angleTo(target);
-					return "translate("
-					+ (source.x + source.radius)
-					+ ","
-					+ (source.y + source.radius)
-					+ ")" + "rotate(" + angle + ")";
-				})
-				.attr("d", function(rl) {
-					let source = that.model.nodes.find( x => x.id == rl.startNode)
-					let target = that.model.nodes.find( x => x.id == rl.endNode)
-					let distance = source.distanceTo(target) - target.radius - 34;
-					return "M " + (source.radius + 12) + " 2.5" + 
-					"L " + distance + " 2.5" + 
-					"L " + distance + " 10"  +
-					"L " + (distance + 20) + " 0" +
-					"L " + distance + " -10" + 
-					"L " + distance + " -2.5" + 
-					"L " + (source.radius + 12) + " -2.5" +
-					"Z";
-				})
-				.attr("fill", function(rel) { return rel.style.fill });
-		// rel.enter()
-		// 	.append("text")
-		// 		.attr("transform", function(rl) {
-		// 			let source = that.model.nodes.find( x => x.id == rl.startNode)
-		// 			let target = that.model.nodes.find( x => x.id == rl.endNode)
-		// 			let angle = source.angleTo(target);
-		// 			return "translate("
-		// 			+ ((source.x + source.radius + target.x + target.radius) / 2 + 25)
-		// 			+ ","
-		// 			+ ((source.y + source.radius + target.y + target.radius) / 2)
-		// 			+ ")" + "rotate(" + angle + ")";
-		// 		})
-		// 		.attr("fill", function(rl) { return rl.style.fill })
-		// 		.attr("class", "node-properties-text")
-		// 		.attr("text-anchor", "middle")
-		// 		.attr("font-size",  "50px")
-		// 		.attr("alignment-baseline", "central")
-		// 		.text(function(rl) { return rl.type; })
-
-		let relOverlay = this.gOverlay.selectAll("path.relationship")
-			.data(this.model.relationships);
-		relOverlay.remove();
-		relOverlay.enter()
-			.append("path")
-				.attr("class", "relationship")
-				.attr("transform", function(rl) {
-					let source = that.model.nodes.find( x => x.id == rl.startNode)
-					let target = that.model.nodes.find( x => x.id == rl.endNode)
-					let angle = source.angleTo(target);
-					return "translate("
-					+ (source.x + source.radius)
-					+ ","
-					+ (source.y + source.radius)
-					+ ")" + "rotate(" + angle + ")";
-				})
-				.attr("d", function(rl) {
-					let source = that.model.nodes.find( x => x.id == rl.startNode)
-					let target = that.model.nodes.find( x => x.id == rl.endNode)
-					let distance = source.distanceTo(target) - target.radius - 34;
-					return "M " + (source.radius + 12) + " 7" + 
-					"L " + (distance - 3) + " 7" + 
-					"L " + (distance - 3) + " 16"  +
-					"L " + (distance + 26) + " 0" +
-					"L " + (distance - 3) + " -16" + 
-					"L " + (distance - 3) + " -7" + 
-					"L " + (source.radius + 12) + " -7" +
-					"Z";
-				})
-				.attr("fill", "rgba(255, 255, 255, 0)")
-				.on("mouseover", mOver)
-				.on("mouseleave", mLeave)
-				.on("click", (rl) => {
-			  	if(this.access != "Read Only"){
-				   	this.currentR = rl;
-				   	this.showNodeTools = false;
-				  	this.showTools = true;
-				  	this.ref.detectChanges();
-			  	}
-			   });
-
 		function mRingOver(){
 			d3.select(this).style("stroke", "rgba(150, 150, 255, 0.5)");
 		}
@@ -365,7 +360,7 @@ export class DiagramComponent implements OnInit {
 				}
 			}
 
-			let distance = n.distanceTo(newNode) - newNode.radius - 34;
+			let distance = n.distanceTo(newNode) - newNode.radius - 12;
 			if(closestNode){
 				d3.select("rect.newNode")
 					.attr("fill", "none")
@@ -392,14 +387,7 @@ export class DiagramComponent implements OnInit {
 						+ ")" + "rotate("
 						+ n.angleTo(newNode) + ")"
 						)
-					.attr("d", "M " + (n.radius + 12) + " 2.5" + 
-						"L " + distance + " 2.5" + 
-						"L " + distance + " 10"  +
-						"L " + (distance + 20) + " 0" +
-						"L " + distance + " -10" + 
-						"L " + distance + " -2.5" + 
-						"L " + (n.radius + 12) + " -2.5" +
-						"Z")
+					.attr("d", that.horizontalArrow(n.radius + 12, distance, 5).outline)
 					.attr("fill", "#333333");			
 			}
 		}
@@ -442,6 +430,149 @@ export class DiagramComponent implements OnInit {
 	}
 
 	/*
+		Arrows
+	*/
+	groupRelationships(){
+		this.groups = [];
+		let gr = {};
+		for(let r of this.model.relationships){
+			let source = this.model.nodes.find( x => x.id == r.startNode);
+			let target = this.model.nodes.find( x => x.id == r.endNode);
+			r["source"] = source;
+			r["target"] = target;
+			r["angle"] = source.angleTo(target);
+			r["distance"] = source.distanceTo(target) - 12;
+			let key = r.startNode < r.endNode ? r.startNode + r.endNode:r.endNode + r.startNode;
+			let g = gr[key]
+			if(g){
+				r["offset"] = g.length * 10;
+				g.push(r)
+			} else {
+				r["offset"] = 0;
+				gr[key] = [r];
+			}
+		}
+		for(let g in gr) { this.groups.push(gr[g])};
+	}
+	horizontalArrow(start, end, arrowWidth) {
+	    let shaftRadius = arrowWidth / 2;
+	    let headRadius = arrowWidth * 2;
+	    let headLength = headRadius * 2;
+	    let shoulder = start < end ? end - headLength : end + headLength;
+	    return {
+	        outline: [
+	            "M", start, shaftRadius,
+	            "L", shoulder, shaftRadius,
+	            "L", shoulder, headRadius,
+	            "L", end, 0,
+	            "L", shoulder, -headRadius,
+	            "L", shoulder, -shaftRadius,
+	            "L", start, -shaftRadius,
+	            "Z"
+	        ].join(" "),
+	        apex: {
+	            "x": start + (shoulder - start) / 2,
+	            "y": 0
+	        }
+	    };
+	} 
+	curvedArrow(startRadius, endRadius, endCentre, minOffset, arrowWidth, headWidth, headLength){
+	  let startAttach, endAttach, offsetAngle;
+
+	  function square(l){ return l * l; }
+
+	  let radiusRatio = startRadius / (endRadius + headLength);
+	  let homotheticCenter = -endCentre * radiusRatio / (1 - radiusRatio);
+
+	  function intersectWithOtherCircle(fixedPoint, radius, xCenter, polarity){
+	      let gradient = fixedPoint.y / (fixedPoint.x - homotheticCenter);
+	      let hc = fixedPoint.y - gradient * fixedPoint.x;
+
+	      let A = 1 + square(gradient);
+	      let B = 2 * (gradient * hc - xCenter);
+	      let C = square(hc) + square(xCenter) - square(radius);
+
+	      let intersection = { "x": (-B + polarity * Math.sqrt( square( B ) - 4 * A * C )) / (2 * A) };
+	      intersection["y"] = (intersection.x - homotheticCenter) * gradient;
+
+	      return intersection;
+	  }
+
+	  if(endRadius + headLength > startRadius){
+	      offsetAngle = minOffset / startRadius;
+	      startAttach = {
+	          x: Math.cos( offsetAngle ) * (startRadius),
+	          y: Math.sin( offsetAngle ) * (startRadius)
+	      };
+	      endAttach = intersectWithOtherCircle( startAttach, endRadius + headLength, endCentre, -1 );
+	  } else {
+	      offsetAngle = minOffset / endRadius;
+	      endAttach = {
+	          x: endCentre - Math.cos( offsetAngle ) * (endRadius + headLength),
+	          y: Math.sin( offsetAngle ) * (endRadius + headLength)
+	      };
+	      startAttach = intersectWithOtherCircle( endAttach, startRadius, 0, 1 );
+	  }
+
+	  let
+	      g1 = -startAttach.x / startAttach.y,
+	      c1 = startAttach.y + (square( startAttach.x ) / startAttach.y),
+	      g2 = -(endAttach.x - endCentre) / endAttach.y,
+	      c2 = endAttach.y + (endAttach.x - endCentre) * endAttach.x / endAttach.y;
+
+	  let cx = ( c1 - c2 ) / (g2 - g1);
+	  let cy = g1 * cx + c1;
+
+	  let arcRadius = Math.sqrt(square(cx - startAttach.x) + square(cy - startAttach.y));
+
+	  function startTangent(dr){
+	      let dx = (dr < 0 ? -1 : 1) * Math.sqrt(square(dr) / (1 + square(g1)));
+	      let dy = g1 * dx;
+	      return [
+	          startAttach.x + dx,
+	          startAttach.y + dy
+	      ].join(",");
+	  }
+
+	  function endTangent(dr){
+	      let dx = (dr < 0 ? -1 : 1) * Math.sqrt(square(dr) / (1 + square(g2)));
+	      let dy = g2 * dx;
+	      return [
+	          endAttach.x + dx,
+	          endAttach.y + dy
+	      ].join(",");
+	  }
+
+	  function endNormal(dc){
+	      let dx = (dc < 0 ? -1 : 1) * Math.sqrt(square(dc) / (1 + square(1 / g2)));
+	      let dy = dx / g2;
+	      return [
+	          endAttach.x + dx,
+	          endAttach.y - dy
+	      ].join(",");
+	  }
+
+	  let shaftRadius = arrowWidth / 2;
+	  let headRadius = headWidth / 2;
+
+	  return {
+	      outline: [
+	          "M", startTangent(-shaftRadius),
+	          "L", startTangent(shaftRadius),
+	          "A", arcRadius - shaftRadius, arcRadius - shaftRadius, 0, 0, minOffset > 0 ? 0 : 1, endTangent(-shaftRadius),
+	          "L", endTangent(-headRadius),
+	          "L", endNormal(headLength),
+	          "L", endTangent(headRadius),
+	          "L", endTangent(shaftRadius),
+	          "A", arcRadius + shaftRadius, arcRadius + shaftRadius, 0, 0, minOffset < 0 ? 0 : 1, startTangent(-shaftRadius)
+	      ].join( " " ),
+	      apex: {
+	          "x": cx,
+	          "y": cy > 0 ? cy - arcRadius : cy + arcRadius
+	      }
+	  };
+	}  
+	/*
 
 		Tools
 		
@@ -466,7 +597,7 @@ export class DiagramComponent implements OnInit {
 			.on("dblclick.zoom", null);
 
     function zoomed() {
-        d3.selectAll("g").attr("transform", d3.event.transform);
+        d3.selectAll("g.layer").attr("transform", d3.event.transform);
     }
 	}
 
@@ -562,6 +693,21 @@ export class DiagramComponent implements OnInit {
 		this.ref.detectChanges();
 	}
 
+	lockMirrorNode(){
+		this.mirrorNode.isLocked = this.mirrorNode.isLocked ? false:true; 
+	}
+	copyMirrorNode(){
+		if(this.mirrorNode.isLocked){
+			this.currentNode.style.color = this.mirrorNode.color;
+			this.currentNode.style.fill = this.mirrorNode.fill;
+			this.currentNode.isRectangle = this.mirrorNode.isRectangle;
+			this.saveNode();
+		} else {
+			this.mirrorNode.color = this.currentNode.style.color;
+			this.mirrorNode.fill = this.currentNode.style.fill;
+			this.mirrorNode.isRectangle = this.currentNode.isRectangle;
+		}
+	}
 
 	/*
 
